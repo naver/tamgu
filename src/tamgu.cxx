67f6a37c6bb152923aa576d224a7c96ed1143b69
Claude Roux <claude.roux@naverlabs.com>
2019-11-21 16:07:06 +0100

    Bug correction
    In some rare cases, a value in a taskell iteration could be void.

--- src/tamgu.cxx
+++ src/tamgu.cxx
@@ -40,7 +40,7 @@
 #include "tamgutaskell.h"
 
 //----------------------------------------------------------------------------------
-const char* tamgu_version = "Tamgu 0.96.4 build 50";
+const char* tamgu_version = "Tamgu 0.96.4 build 51";
 
 Tamgu* booleantamgu[2];
 
