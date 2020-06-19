c81389434794c1d60318da2aadfb47e867b92fc3
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2020-06-19 18:51:49 +0200

    Bug on Lisp function: key
    a should not be protected in this context

--- src/tamgulisp.cxx
+++ src/tamgulisp.cxx
@@ -1348,7 +1348,6 @@ Tamgu* Tamgulisp::Eval(Tamgu* contextualpattern, Tamgu* v0, short idthread) {
                 if (sz == 3) {
                     //We simply return the value...
                     a = v0->Eval(contextualpattern, &idx, idthread);
-                    a->Protect();
                     v0->Releasenonconst();
                     v1->Releasenonconst();
                     return a;
@@ -1380,7 +1379,6 @@ Tamgu* Tamgulisp::Eval(Tamgu* contextualpattern, Tamgu* v0, short idthread) {
                 if (sz == 4) {
                     //We simply return the value...
                     a = v0->Eval(contextualpattern,&idx, idthread);
-                    a->Protect();
                     v0->Releasenonconst();
                     v1->Releasenonconst();
                     v2->Releasenonconst();
