ab742fa1e59e94af8c31a28244dca89aff1fc6da
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2020-06-24 16:57:24 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/sh
 echo ------------------------------------------------------------------
 echo
-echo To compile the Python library: provide the python version as: "3.7m"
+echo Compiling tamgu
 echo
+echo To compile the Python library (example):
+echo 	-pythoninclude /usr/include/python3.7m -pythonversion 3.7m
 echo ------------------------------------------------------------------
 
 if [ "-h" = "$1" ]; then
@@ -20,9 +22,5 @@ if [ "--help" = "$1" ]; then
    exit
 fi
 
-if [ "" = "$1" ]; then
-   python install.py -withfastint -avx $1 $2 $3 $4 $5 $6 $7 $8 $9
-else
-   python install.py -withfastint -avx -pythoninclude /usr/include/python$1 -pythonversion $1 $2 $3 $4 $5 $6 $7 $8 $9
-fi
+python install.py -withfastint -avx $1 $2 $3 $4 $5 $6 $7 $8 $9
 
