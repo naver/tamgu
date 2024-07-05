42d235017d7a05e7694a18f519b5abaab45b2907
Claude ROUX[Systemic AI] <claude.roux@naverlabs.com>
2024-07-05 11:15:38 +0200

    update package vscode

--- vscode/out/extension.js
+++ vscode/out/extension.js
@@ -4,25 +4,48 @@ exports.activate = activate;
 exports.deactivate = deactivate;
 const vscode = require("vscode");
 function activate(context) {
-    let disposable = vscode.commands.registerCommand('extension.runTamgu', () => {
-        const editor = vscode.window.activeTextEditor;
-        if (editor) {
-            const document = editor.document;
-            const filePath = document.fileName;
-            if (filePath.endsWith('.tmg')) {
-                const terminal = vscode.window.createTerminal('Tamgu');
-                terminal.show();
-                terminal.sendText(`tamgu "${filePath}"`);
-            }
-            else {
-                vscode.window.showErrorMessage('The current file is not a Tamgu (.tmg) file.');
-            }
-        }
-        else {
-            vscode.window.showErrorMessage('No active editor found.');
+    let disposableCommand = vscode.commands.registerCommand('extension.runTamgu', () => {
+        runCurrentTamguFile();
+    });
+    context.subscriptions.push(disposableCommand);
+    let disposableOpen = vscode.workspace.onDidOpenTextDocument((document) => {
+        if (document.fileName.endsWith('.tmg')) {
+            runTamguFile(document.fileName);
         }
     });
-    context.subscriptions.push(disposable);
+    let disposableSave = vscode.workspace.onDidSaveTextDocument((document) => {
+        if (document.fileName.endsWith('.tmg')) {
+            runTamguFile(document.fileName);
+        }
+    });
+    context.subscriptions.push(disposableOpen);
+    context.subscriptions.push(disposableSave);
+}
+function runCurrentTamguFile() {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const document = editor.document;
+        const filePath = document.fileName;
+        runTamguFile(filePath);
+    }
+    else {
+        vscode.window.showErrorMessage('No active editor found.');
+    }
+}
+function runTamguFile(filePath) {
+    if (filePath.endsWith('.tmg')) {
+        // Chercher un terminal existant nommé "Tamgu"
+        let terminal = vscode.window.terminals.find(t => t.name === 'Tamgu');
+        // Si le terminal "Tamgu" n'existe pas, créer un nouveau terminal
+        if (!terminal) {
+            terminal = vscode.window.createTerminal('Tamgu');
+        }
+        terminal.show();
+        terminal.sendText(`tamgu "${filePath}"`);
+    }
+    else {
+        vscode.window.showErrorMessage('The current file is not a Tamgu (.tmg) file.');
+    }
 }
 function deactivate() { }
 //# sourceMappingURL=extension.js.map
